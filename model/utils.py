158e7a6916c32fda48653d0edb37436a72c89582
Andrew <andrew@192.168.2.2>
2024-11-16 20:41:50 +0700

    20411611 update analysis

--- model/utils.py
+++ model/utils.py
@@ -3,7 +3,3 @@ import pandas as pd
 import re
 from sklearn.model_selection import train_test_split
 
-def clean_content(text):
-    text = re.sub(r'\s+', ' ', text)
-    text = re.sub(r'[^\w\s]', '', text)
-    return text.strip().lower()
